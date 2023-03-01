cb4b2f958ac8828233c17c9315c3e4910e575145
Doreen2002 <doreenmwapekatebe8@gmail.com>
2023-03-01 13:44:05 +0200

    changed fetching link to dynamic

--- desk/src/pages/desk/CustomerLedger.vue
+++ desk/src/pages/desk/CustomerLedger.vue
@@ -2,12 +2,12 @@
 	<div class = "flex flex-col h-full px-4 overflow-hidden">
 		<div class="flex flex-row py-10 px-8">
 	<h1 class="text-blue-700 px-8 py-4">Add  filters</h1>
-	<select class="block w-64 h-16 p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+	<select class="block w-64 h-8 p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 		<option>Customer</option>
 	</select>
 	<span class="py-4 px-4">Equals</span>
-	<input type="text" class="h-16 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="filters">
-	<button class="h-16 w-64 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" @click="getGeneralLedger">Filter</button>
+	<input type="text" class="h-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="filters">
+	<button class="h-8 w-64 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" @click="getGeneralLedger">Filter</button>
 		</div>
 	
 		<div>
@@ -55,7 +55,7 @@ import ListManager from "@/components/global/ListManager.vue"
 	methods: {
 		getGeneralLedger: function()
 		{
-		fetch(`http://learning:8005/api/resource/GL Entry/?fields=["posting_date","company","account", "party_type", "party", "debit","credit", "against", "against_voucher_type", "against_voucher", "voucher_type", "voucher_no"]&&filters={"party":"${this.filters}"}`, { method: "GET", headers: {
+		fetch(`http://${window.location.hostname}:${ window.location.port}/api/resource/GL Entry/?fields=["posting_date","company","account", "party_type", "party", "debit","credit", "against", "against_voucher_type", "against_voucher", "voucher_type", "voucher_no"]&&filters={"party":"${this.filters}"}`, { method: "GET", headers: {
                 "Authorization": "token 88a44253a62631a:cd57aa6393d60a0",
             },
         })
